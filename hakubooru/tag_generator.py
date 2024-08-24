842c57de51edff1399ced20d4026080c96c890ae
Koishi-Star <66173435+Koishi-Star@users.noreply.github.com>
2024-08-24 08:54:05 +0800

    Update tag_generator.py

--- hakubooru/tag_generator.py
+++ hakubooru/tag_generator.py
@@ -7,6 +7,7 @@ from hakubooru.metainfo import (
     rating_map,
     fav_count_percentile_full,
 )
+from datetime import datetime, timezone
 
 
 def year_tag(
@@ -48,24 +49,30 @@ def quality_tag(
     general_tags: list[str],
     percentile_map: dict[str, dict[int, int]] = fav_count_percentile_full,
 ) -> tuple[list[str], list[str]]:
-    if post.id > 7000000:
-        # Don't add quality tag for posts which are new.
-        return keep_tags, general_tags
+    # 为了能返回判断值，我们不再从这里截断函数
+    if post.id < 5021000:
+        # id=5021000为2022年1月1日最新一张图片
+        score = post.fav_count
     rating = post.rating
-    score = post.fav_count
     percentile = percentile_map[rating]
 
-    if score > percentile[95]:
-        quality_tag = "masterpiece"
-    elif score > percentile[85]:
+    year = 0
+    start_date = datetime(2022, 1, 1, tzinfo=timezone.utc)
+    try:
+        date = dateutil.parser.parse(post.created_at).replace(tzinfo=timezone.utc)
+        year = date.year
+        if year >= 2022:
+            score = post.fav_count
+            days_since_start = (date - start_date).total_seconds() / (24 * 60 * 60)
+            score = (40 / (-0.034 * days_since_start + 48.3426)) * score
+    except:
+        pass
+
+    if score > percentile[85]:
         quality_tag = "best quality"
-    elif score > percentile[75]:
-        quality_tag = "great quality"
-    elif score > percentile[50]:
-        quality_tag = "good quality"
-    elif score > percentile[25]:
+    elif score > percentile[65]:
         quality_tag = "normal quality"
-    elif score > percentile[10]:
+    elif score > percentile[35]:
         quality_tag = "low quality"
     else:
         quality_tag = "worst quality"
@@ -81,28 +88,33 @@ def quality_tag_new(
     general_tags: list[str],
     percentile_map: dict[str, dict[int, int]] = fav_count_percentile_full,
 ) -> tuple[list[str], list[str]]:
-    if post.id > 7500000:
+    if post.id < 5021000:
         # Don't add quality tag for posts which are new.
-        return keep_tags, general_tags
+        score = post.fav_count
+
+    start_date = datetime(2022, 1, 1, tzinfo=timezone.utc)
     rating = post.rating
-    score = post.fav_count
     percentile = percentile_map[rating]
 
-    if score > percentile[90]:
-        quality_tag = "masterpiece"
-    elif score > percentile[75]:
+    try:
+        date = dateutil.parser.parse(post.created_at).replace(tzinfo=timezone.utc)
+        year = date.year
+        if year >= 2022:
+            score = post.fav_count
+            days_since_start = (date - start_date).total_seconds() / (24 * 60 * 60)
+            score = round((40 / (-0.034 * days_since_start + 48.3426)) * score, 1)
+    except:
+        pass
+
+    if score > percentile[85]:
         quality_tag = "best quality"
-    elif score > percentile[60]:
-        quality_tag = "great quality"
-    elif score > percentile[45]:
-        quality_tag = "good quality"
-    elif score > percentile[30]:
+    elif score > percentile[65]:
         quality_tag = "normal quality"
-    elif score > percentile[10]:
+    elif score > percentile[35]:
         quality_tag = "low quality"
     else:
         quality_tag = "worst quality"
-
     general_tags.append(quality_tag)
+    # general_tags.append(str(score))
 
     return keep_tags, general_tags
